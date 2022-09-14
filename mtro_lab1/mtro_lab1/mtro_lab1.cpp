c9c9a554fb1b19e11dd69866e572e9b7b10df6eb
aleshkey <lesha.and.android@gmail.com>
2022-09-14 08:22:55 +0300

    metrology

---
+++ mtro_lab1/mtro_lab1/mtro_lab1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, m, sum=1;
+
+	cin >> a>>m;
+
+	for (int i = 1; i <= m; i++) {
+		sum += a * i;
+	}
+
+	cout << sum;
+
+}
+
+
+/*
+	максимальное время выполнения - 14сек
+	кол-во переменных - 4
+*/
